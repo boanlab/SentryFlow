b1cce6d51dd93a04feab0c0b980e52bec4b935ca
Isu Kim <49092508+isu-kim@users.noreply.github.com>
2024-01-16 14:16:32 +0900

    Fix bug with ^M
    - fix: bug with shell getting ^M as new line since the file was from Windows

--- k8s/inject-sidecars.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Get namespaces using kubectl and exclude the headers
-kubectl get ns --no-headers | awk '{ if ($1 != "kube-system" && $1 != "istio-system" && $1 != "metallb-system" ) print $1 }' | xargs -I {} kubectl label namespace {} istio-injection=enabled --overwrite && kubectl get deployment --all-namespaces -o custom-columns=:metadata.namespace,:metadata.name --no-headers | awk '{ if ($1 != "kube-system" && $1 != "istio-system" && $1 != "metallb-system" ) print "kubectl rollout restart deployment -n " $1 " " $2 }' | sh
-
