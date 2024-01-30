261171f4981627eb3e7f04ce4f81c4e0b8e2bfb6
csb0710 <sdssond@gmail.com>
2024-01-30 18:47:59 +0900

    Refactor struct

---
+++ src/metric/metric.go
@@ -0,0 +1,41 @@
+package main
+
+import protobuf "otel-custom-collector/protobuf"
+
+type APINode struct {
+	path  string
+	count int
+	child []*APINode
+}
+
+type classifiedAPI struct {
+	destination string
+	method      string
+	URIRoot     *APINode
+}
+
+func classifyAPI(accessLogs []protobuf.AccessLog) { // return classifiedAPI
+}
+
+func generateMetric(cal classifiedAPI) {
+
+}
+
+// Functions for generating metrics.
+func statisticOfAPIsPerDestination(cal classifiedAPI) {
+
+}
+
+func statisticOfAPIsPerMin(cal classifiedAPI) {
+
+}
+
+func statisticOfErrorAPI(cal classifiedAPI) {
+
+}
+
+func statisticOfAPILatency(cal classifiedAPI) {
+
+}
+
+// plan to add features
