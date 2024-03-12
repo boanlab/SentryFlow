1407b7c6b1e34cbca5ebc4b7dd2cbc7981d6575f
csb0710 <sdssond@gmail.com>
2024-03-12 10:30:10 +0000

    Update Exporter Log

--- sentryflow/exporter/exporterServer.go
+++ sentryflow/exporter/exporterServer.go
@@ -28,7 +28,7 @@ func NewExporterServer() *Server {
 
 // GetLog Function
 func (exs *Server) GetLog(info *protobuf.ClientInfo, stream protobuf.SentryFlow_GetLogServer) error {
-	log.Printf("[Exporter] Client %s(%s) connected (GetEnvoyMetrics)", info.HostName, info.IPAddress)
+	log.Printf("[Exporter] Client %s(%s) connected (GetLog)", info.HostName, info.IPAddress)
 
 	curExporter := &Inform{
 		stream:    stream,
@@ -67,7 +67,7 @@ func (exs *Server) GetEnvoyMetrics(info *protobuf.ClientInfo, stream protobuf.Se
 
 // GetAPIMetrics Function
 func (exs *Server) GetAPIMetrics(_ context.Context, info *protobuf.ClientInfo) (*protobuf.APIMetric, error) {
-	log.Printf("[Exporter] Client %s(%s) connected", info.HostName, info.IPAddress)
+	log.Printf("[Exporter] Client %s(%s) connected (GetAPIMetrics)", info.HostName, info.IPAddress)
 
 	// Construct protobuf return value
 	ret := protobuf.APIMetric{
