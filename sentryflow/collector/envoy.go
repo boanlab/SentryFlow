b0605f3b3e21a30ca36d6cbd780f30d151ef096f
Isu Kim <49092508+isu-kim@users.noreply.github.com>
2024-03-12 13:57:18 +0900

    fix bug with metric not gathered periodically

--- sentryflow/collector/envoy.go
+++ sentryflow/collector/envoy.go
@@ -32,55 +32,55 @@ func (ems *EnvoyMetricsServer) registerService(server *grpc.Server) {
 
 // StreamMetrics Function
 func (ems *EnvoyMetricsServer) StreamMetrics(stream envoyMetrics.MetricsService_StreamMetricsServer) error {
-	for {
-		event, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			log.Printf("[Envoy] Something went on wrong when receiving event: %v", err)
-			return err
-		}
-
-		err = event.ValidateAll()
-		if err != nil {
-			log.Printf("[Envoy] Failed to validate stream: %v", err)
-		}
-
-		// @todo parse this event entry into our format
-		identifier := event.GetIdentifier()
-		identifier.GetNode().GetMetadata()
-
-		if identifier != nil {
-			log.Printf("[Envoy] Received EnvoyMetric - ID: %s, %s", identifier.GetNode().GetId(), identifier.GetNode().GetCluster())
-
-			nodeID := identifier.GetNode().GetId()
-			cluster := identifier.GetNode().GetCluster()
-
-			curIdentifier := fmt.Sprintf("%s, %s", nodeID, cluster)
-			envoyMetric := &protobuf.EnvoyMetric{
-				Identifier: curIdentifier,
-				Metric:     []*protobuf.Metric{},
-			}
-
-			for _, metric := range event.GetEnvoyMetrics() {
-				metricType := metric.GetType().String()
-				metricName := metric.GetName()
-				tempMetrics := metric.GetMetric()
-				metrics := fmt.Sprintf("%s", tempMetrics)
-
-				curMetric := &protobuf.Metric{
-					Type:  metricType,
-					Key:   metricName,
-					Value: metrics,
-				}
-
-				envoyMetric.Metric = append(envoyMetric.Metric, curMetric)
-			}
-
-			core.Lh.InsertLog(envoyMetric)
-		}
+	event, err := stream.Recv()
+	if err == io.EOF {
+		return nil
 	}
+	if err != nil {
+		log.Printf("[Envoy] Something went on wrong when receiving event: %v", err)
+		return err
+	}
+
+	err = event.ValidateAll()
+	if err != nil {
+		log.Printf("[Envoy] Failed to validate stream: %v", err)
+	}
+
+	// @todo parse this event entry into our format
+	identifier := event.GetIdentifier()
+	identifier.GetNode().GetMetadata()
+
+	if identifier != nil {
+		log.Printf("[Envoy] Received EnvoyMetric - ID: %s, %s", identifier.GetNode().GetId(), identifier.GetNode().GetCluster())
+
+		nodeID := identifier.GetNode().GetId()
+		cluster := identifier.GetNode().GetCluster()
+
+		curIdentifier := fmt.Sprintf("%s, %s", nodeID, cluster)
+		envoyMetric := &protobuf.EnvoyMetric{
+			Identifier: curIdentifier,
+			Metric:     []*protobuf.Metric{},
+		}
+
+		for _, metric := range event.GetEnvoyMetrics() {
+			metricType := metric.GetType().String()
+			metricName := metric.GetName()
+			tempMetrics := metric.GetMetric()
+			metrics := fmt.Sprintf("%s", tempMetrics)
+
+			curMetric := &protobuf.Metric{
+				Type:  metricType,
+				Key:   metricName,
+				Value: metrics,
+			}
+
+			envoyMetric.Metric = append(envoyMetric.Metric, curMetric)
+		}
+
+		core.Lh.InsertLog(envoyMetric)
+	}
+
+	return nil
 }
 
 // EnvoyAccessLogsServer Structure
